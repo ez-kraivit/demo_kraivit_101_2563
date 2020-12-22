51b69d77335e49d7e526745c66278dc46e86c34f
Kraivit Mongkhonsakunrit <kraivit.m@edvisory.co.th>
2020-12-22 17:36:38 +0700

    Update 22

---
+++ index.js
@@ -0,0 +1,11 @@
+class demo_kraivit_101{
+    static demo = (name) => {
+        return name
+    }
+    static demo_1 = ()=> {
+        const x = () => {return 'hello'}
+        x()
+    }
+}
+
+module.exports = demo_kraivit_101;
